054498696f784e4494bd46f3b95a4a033586a345
djagusgh <35323742+djagusgh@users.noreply.github.com>
2019-06-23 01:37:48 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ from flask_twisted import Twisted
 from twisted.python import log
 
 if __name__ == "__main__":
+    
+    app = create_app()
     twisted = Twisted(app)
     log.startLogging(sys.stdout)
 
